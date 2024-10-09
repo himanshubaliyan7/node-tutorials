ff7be5428c70bcec469bce1b0f51ca822dc6e92f
Himanshu Baliyan <himanshubaliyan4000@gmail.com>
2024-10-09 22:29:01 +0530

    ADD Module 1.5 Middleware

--- 1.4 Express Server/index.js
+++ 1.4 Express Server/index.js
@@ -4,8 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send('Hello World! from Himanshu.');
-    console.log(req);
+    res.send('Hello World! from Himanshu Baliyan.');
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
+});
+
+app.put("/user/himanshu", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/himanshu", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/himanshu", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
